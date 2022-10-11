669fc4c09fd6c1d86fb2cb11daff9562c8ccc76f
Wallace Nascimento <wallacediogo10@hotmail.com>
2022-10-11 18:41:54 -0300

    learned config and params

--- ClassAnatomy/src/MyClass.java
+++ ClassAnatomy/src/MyClass.java
@@ -1,3 +1,6 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class MyClass {
     // public static void main(String[] args) {
     //     System.out.println("Aoba!");
@@ -12,12 +15,21 @@ public class MyClass {
     // }
 
     public static void main(String [] args) {
-        String name1 = "Wallace";
-        String name2 = "Nascimento";
+        String name1 = args [0];
+        //recebe esses parâmetros ao ser executado
+        String name2 = args [1];
 
         String fullName = completeName(name1, name2);
 
         System.out.println(fullName);
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("digite sua idade:\t");
+        int idade = scanner.nextInt();
+
+        System.out.println("você tem " + idade + " anos!");
+
     }
     
     public static String completeName (String name1, String name2) {
